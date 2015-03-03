cfd6b2ed4ae9c32fa05f746701ce1d0f81dc79a1
Rich Shea <richcshea@gmail.com>
2015-03-02 19:58:21 -0700

    add table to homepage. add hard coded date to table.  add link for each city in table

--- app/controllers/welcomes_controller.rb
+++ app/controllers/welcomes_controller.rb
@@ -1,4 +1,7 @@
 class WelcomesController < ApplicationController
   def index
+    cities = %w(Denver Chicago Austin Orlando Philadelphia Boston Houston Dallas Oakland Tampa)
+    emps = [25, 20, 18, 17, 15, 15, 7, 6, 5, 1]
+    @jobs = cities.zip(emps)
   end
 end
