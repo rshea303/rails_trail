c4c972ded75e2958f324b362d5946ee4e17682e2
Rich Shea <richcshea@gmail.com>
2015-03-02 20:34:31 -0700

    add top ten pay table

--- app/controllers/welcomes_controller.rb
+++ app/controllers/welcomes_controller.rb
@@ -2,6 +2,7 @@ class WelcomesController < ApplicationController
   def index
     cities = %w(Denver Chicago Austin Orlando Philadelphia Boston Houston Dallas Oakland Tampa)
     emps = [25, 20, 18, 17, 15, 15, 7, 6, 5, 1]
-    @jobs = cities.zip(emps)
+    salary = %w($125000 $115000 $100000 $97000 $95000 $90000 $87000 $78000 $76000 $65000)
+    @jobs = cities.zip(emps, salary)
   end
 end
