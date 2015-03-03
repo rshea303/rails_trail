45d028414f1556f86e93755a04b1caf1391729dd
Rich Shea <richcshea@gmail.com>
2015-03-02 20:58:21 -0700

    add top ten remote jobs

--- app/controllers/welcomes_controller.rb
+++ app/controllers/welcomes_controller.rb
@@ -3,6 +3,7 @@ class WelcomesController < ApplicationController
     cities = %w(Denver Chicago Austin Orlando Philadelphia Boston Houston Dallas Oakland Tampa)
     emps = [25, 20, 18, 17, 15, 15, 7, 6, 5, 1]
     salary = %w($125000 $115000 $100000 $97000 $95000 $90000 $87000 $78000 $76000 $65000)
-    @jobs = cities.zip(emps, salary)
+    remotes = %w(Quickleft Thoughbot Aetna Bonobos IBM Google DISH Tek Cyber Disney)
+    @jobs = cities.zip(emps, salary, remotes)
   end
 end
